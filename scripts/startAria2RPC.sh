a00d9d4e0a74ef5caa309c767d83cde141742189
temp25 <39752837+temp25@users.noreply.github.com>
2022-08-30 13:17:09 +0530

    Updated path changes

--- scripts/startAria2RPC.sh
+++ scripts/startAria2RPC.sh
@@ -4,34 +4,43 @@ ARIA2C_RPC_SERVER_PID=$(ps -ef | grep aria2c | grep -v grep | awk '{print $2}')
 
 if [ -z "$ARIA2C_RPC_SERVER_PID" ]; then
     ARIA2C_BUILD_NAME=aria2c.tar.bz2
-    
+
     echo "Downloading Aria2c static build latest release from q3aql/aria2-static-builds"
     curl -s https://api.github.com/repos/q3aql/aria2-static-builds/releases/latest | grep "download_url" | grep "32bit-build1" | grep "tar" | cut -d : -f 2,3 | tr -d \" | wget -O $ARIA2C_BUILD_NAME -qi- - 
-    
+
     echo "Extracting aria2c static build extract $ARIA2C_BUILD_NAME"
     tar xjf $ARIA2C_BUILD_NAME --strip-components=1 --wildcards **/aria2c **/ca-certificates.crt
-    
+
+    PWD="$(pwd)"
+
+    echo -e "\nCurrent working directory, ${PWD}\n"
+
+    echo -e "\nListing directory contents\n"
+    ls -lah "${PWD}"
+
     RESULT=$?
     if [ $RESULT -ne 0 ]; then
         echo "Error occurred in extracting $ARIA2C_BUILD_NAME"
         exit 22 # terminate and indicate error
     fi
-    
+
     echo "Extraction completed successfully"
     echo "Removing build extract $ARIA2C_BUILD_NAME"
-    
+
     rm -f $ARIA2C_BUILD_NAME
     echo "Removed build extract $ARIA2C_BUILD_NAME"
-    
+
     #converting static binaries to executables
     chmod +x aria2c
-    
-    mkdir /app/public/downloads
-    touch aria_session.txt
+
+    mkdir "${PWD}/downloads"
+    touch "${PWD}/aria_session.txt"
+
+    mkdir -p "${PWD}/public/downloads"
 
     # nohup ./aria2c --conf-path=aria2c.config > /dev/null 2>&1 < /dev/null &
     # nohup ./aria2c --enable-rpc --rpc-listen-all --log=/app/public/aria2c.log --ca-certificate=/app/ca-certificates.crt --dir=/app/public/downloads --input-file=aria_session.txt --save-session=aria_session.txt --save-session-interval=3 --max-connection-per-server=16 > /dev/null 2>&1 < /dev/null &
-    nohup ./aria2c --enable-rpc --rpc-listen-all --log=/app/public/aria2c.log --check-certificate=false --dir=/app/public/downloads --input-file=aria_session.txt --save-session=aria_session.txt --save-session-interval=3 --max-connection-per-server=16 > /dev/null 2>&1 &
+    nohup ${PWD}/aria2c --enable-rpc --rpc-listen-all --log=${PWD}/public/aria2c.log --check-certificate=false --dir=${PWD}/public/downloads --input-file=aria_session.txt --save-session=aria_session.txt --save-session-interval=3 --max-connection-per-server=16 > /dev/null 2>&1 &
     
     ARIA2C_RPC_SERVER_PID=$!
     echo "Aria2c RPC server started with pid, $ARIA2C_RPC_SERVER_PID"
